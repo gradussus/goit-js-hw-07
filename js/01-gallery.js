b02ba262fe9956c535f3f1fb38b14fc2177a8bf8
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-08-25 16:37:21 +0300

    Импорт картинок

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const placeToImport = document.querySelector('.gallery')
+const createCards = createPreviewCards(galleryItems)
+
+function createPreviewCards(img) {
+    return img.map(({ preview, description }) => { return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+    src="${preview}"
+        data-source="large-image.jpg"
+        alt="${description}"
+      />
+    </a>
+  </div>` }
+ 
+    ).join('')
+}
+placeToImport.insertAdjacentHTML('beforeend', createCards)
