cf1d7a08c8bf8879166110becdaa35abb9848e8e
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-08-27 12:33:03 +0300

    123

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const placeToImport = document.querySelector('.gallery')
-const createCards = createPreviewCards(galleryItems)
+const placeToImport = document.querySelector(".gallery");
+placeToImport.addEventListener("click", onGalleryClick);
+const createCards = createPreviewCards(galleryItems);
 
 function createPreviewCards(img) {
-    return img.map(({ preview, description }) => { return `<div class="gallery__item">
+  return img
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
     <a class="gallery__link" href="large-image.jpg">
       <img
         class="gallery__image"
-    src="${preview}"
-        data-source="large-image.jpg"
+        src="${preview}"
+        data-source="${original}"
         alt="${description}"
       />
     </a>
-  </div>` }
- 
-    ).join('')
+  </div>`;
+    })
+    .join("");
 }
-placeToImport.insertAdjacentHTML('beforeend', createCards)
+placeToImport.insertAdjacentHTML("beforeend", createCards);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  console.log(event.target);
+  const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`);
+
+  instance.show();
+}
